ad2e9abf61b9b7663798dfdb117ce9207e038fa8
JuanS <55510219+sjjssl@users.noreply.github.com>
2022-06-20 20:32:55 -0500

    Add files via upload

--- hackerrank.sql
+++ hackerrank.sql
@@ -487,3 +487,14 @@ FROM
     FROM
         students) AS s
 ORDER BY grade DESC , name;
+
+#Harry Potter and his friends are at Ollivander's with Ron, finally replacing Charlie's old broken wand. 
+#Hermione decides the best way to choose is by determining the minimum number of gold galleons needed to buy each non-evil wand of high 
+#power and age. Write a query to print the id, age, coins_needed, and power of the wands that Ron's interested in, 
+#sorted in order of descending power. If more than one wand has same power, sort the result in order of descending age.
+SELECT id, age, coins_needed,power 
+FROM
+(SELECT id,age,coins_needed,power,ROW_NUMBER() OVER(PARTITION BY power,age ORDER BY coins_needed) AS rn 
+FROM wands w JOIN wands_property wp USING(code) WHERE is_evil=0)
+WHERE rn=1 
+ORDER BY power DESC,age DESC;
